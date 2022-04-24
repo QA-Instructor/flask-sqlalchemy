be583cc26344ff6d303e1e7aba9a05af5d62f200
Jody Broad <msjodybroad@gmail.com>
2022-04-24 13:36:42 +0100

    tidying up routes code

--- application/routes.py
+++ application/routes.py
@@ -19,8 +19,6 @@ def email_signup_form():
 
     if request.method == 'POST':
         email = form.email.data
-
-
         if len(email) == 0:
             error = "Please supply email address"
         else:
@@ -29,119 +27,6 @@ def email_signup_form():
             db.session.commit()
             return 'Thank you!'
     return render_template('home.html', form=form, message=error, title='home')
-
-# Victoria's code
-# def register_basic_form():
-#     error = ""
-#     form = BasicForm()
-#
-#     if request.method == 'POST':
-#         first_name = form.first_name.data
-#         last_name = form.last_name.data
-#
-#         if len(first_name) == 0 or len(last_name) == 0:
-#             error = "Please supply both first and last name"
-#         else:
-#             person = Person(first_name=first_name, last_name=last_name)
-#             db.session.add(person)
-#             db.session.commit()
-#             return 'Thank you!'
-#     return render_template('home.html', form=form, message=error, title='home')
-
-
-
-
-@app.route('/people', methods=['GET'])
-def show_people():
-    error = ""
-    people = Person.query.all()
-    if len(people) == 0:
-        error = "There are no people to display"
-        print(people)
-    return render_template('people.html', people=people, message=error)
-
-
-# @app.route('/cars', methods=['GET'])
-# def show_cars():
-#     error = ""
-#     cars = Car.query.all()
-#     if len(cars) == 0:
-#         error = "There are no cars to display"
-#         print(cars)
-#     return render_template('cars.html', cars=cars, message=error, title="Car")
-
-
-@app.route('/people/<int:person_id>', methods=['GET'])
-def show_person(person_id):
-    error = ""
-    # use filter_by for any column
-    # person = Person.query.filter_by(id=person_id).first()
-    #  use get for the PK
-    person = Person.query.get(person_id)
-
-    # simpsons = Person.query.filter_by(last_name="simpson").all()
-
-    # to sort
-    # simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name).all()
-    # descending sort
-    # simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name.desc()).all()
-    # limit to top 2 simpsons
-    simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name).limit(2).all()
-    if not person:
-        error = "There is no person with ID: " + str(person_id)
-        print(person)
-    return render_template('person.html', person=person, message=error, title="Person", family=simpsons)
-
-
-@app.route('/people/<int:person_id>', methods=['PUT'])
-def update_person(person_id):
-    error = ""
-    person = Person.query.get(person_id)
-    person.last_name = "Flanders"
-    db.session.commit()
-    if not person:
-        error = "There is no person with ID: " + str(person_id)
-        print(person)
-    return render_template('person.html', person=person, message=error, title="Person", family=[])
-
-
-@app.route('/people/<int:person_id>/<string:new_last_name>', methods=['PUT'])
-def update_person_with_name(person_id, new_last_name):
-    error = ""
-    person = Person.query.get(person_id)
-    person.last_name = new_last_name
-    db.session.commit()
-    if not person:
-        error = "There is no person with ID: " + str(person_id)
-        print(person)
-    return render_template('person.html', person=person, message=error, title="Updated Person", family=[])
-
-
-@app.route('/people/<int:person_id>', methods=['DELETE'])
-def delete_person(person_id):
-    error = ""
-    person = Person.query.get(person_id)
-    db.session.delete(person)
-    db.session.commit()
-    people = Person.query.all()
-    if not person:
-        error = "There is no person with ID: " + str(person_id)
-        # print(person)
-    return render_template('people.html', people=people, message=error, title="People")
-
-
-@app.route('/personandcars/<int:person_id>', methods=['GET'])
-def people_and_cars(person_id):
-    error = ""
-    person = Person.query.get(person_id)
-    # cars= person.cars
-    if not person:
-        error = "There is no person with ID: " + str(person_id)
-        print(person)
-        # print(person_and_carinfo)
-    return render_template('person_and_cars.html', person=person, message=error, title="Person and Car Info")
-
-    # return render_template('home.html', form=form, message=error)
 
 
 # LINKS TO PLANT HTML PAGES
@@ -154,11 +39,6 @@ def about():
 @app.route('/contact_us', methods=['GET'])
 def contact():
     return render_template('contact_us.html', title='Contact Us')
-
-
-# @app.route('/plant_care', methods=['GET'])
-# def plant_care():
-#     return render_template('plant_care.html', title='Plant Care')
 
 
 @app.route('/shop', methods=['GET'])
@@ -219,9 +99,7 @@ def plant10():
 # CUSTOMER RELATED ROUTES:
 
 # REGISTERING A NEW CUSTOMER:
-# almost functional, just not linking the user_login_id for some unknown reason, is definitely writing to both the
-# userlogin and address tables, but failing to write to the person because it doesn't have the userlogin id
-# (but it is getting the address one)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -286,53 +164,6 @@ def register():
     return render_template('register.html', title='Register', message=error, form=form)
 
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     error = ""
-#     form = RegistrationForm()
-#
-#     if request.method == 'POST':
-#         first_name = form.first_name.data
-#         last_name = form.last_name.data
-#         email = form.email.data
-#         address_line_one = form.address_line_one.data
-#         address_line_two = form.address_line_two.data
-#         address_line_three = form.address_line_three.data
-#         postcode = form.postcode.data
-#         # username = form.username.data
-#         # password = form.password.data
-#
-#
-# # here would need to also add in username and password
-#         # or len(password) < 4
-#         # or len(username0 == 0
-#
-#         if len(first_name) == 0 \
-#                 or len(last_name) == 0 \
-#                 or len(email) == 0\
-#                 or len(address_line_one) == 0\
-#                 or len(address_line_two) == 0\
-#                 or len(address_line_three) == 0\
-#                 or len(postcode) == 0:
-#             error = "Please complete each section of this form"
-#         else:
-#             address = Address(address_line_one=address_line_one,
-#                               address_line_two=address_line_two,
-#                               address_line_three=address_line_three,
-#                               postcode=postcode)
-#             person = Person(first_name=first_name,
-#                                 last_name=last_name,
-#                                 email=email,
-#                                 address=address)
-#                                 # username=username,
-#                                 # password=password,
-#             db.session.add(address)
-#             db.session.add(person)
-#             db.session.commit()
-#             return 'Thank you'
-#     return render_template('register.html', title='Register', message= error, form=form)
-
-
 # ACCESSING A LIST OF CUSTOMERS
 # This is functional
 
@@ -347,21 +178,9 @@ def show_customers():
     return render_template('customer_list.html', customer=customer, message=error)
 
 
-# @app.route('/customer_list', methods=['GET'])
-# def show_customers():
-#     error = ""
-#     customer = Customer.query.all()
-#     if len(customer) == 0:
-#         error = "There are no people to display"
-#         print(customer)
-#     return render_template('customer_list.html', customer=customer, message=error)
-
-
-
 # STAFF RELATED ROUTES
 
 # REGISTERING A NEW MEMBER OF STAFF:
-# This is almost functional, just not passing back the userlogin_id and the staff_info_id but it is doing the address_id
 
 
 @app.route('/register_staff', methods=['GET', 'POST'])
@@ -420,58 +239,6 @@ def register_staff():
             return render_template('home.html', title='Home', message=error, form=form)
     return render_template('register_staff.html', title='Register New Staff', message=error, form=form)
 
-# @app.route('/register_staff', methods=['GET', 'POST'])
-# def register_staff():
-#     error = ""
-#     form = StaffForm()
-#
-#     if request.method == 'POST':
-#         first_name = form.first_name.data
-#         last_name = form.last_name.data
-#         email = form.email.data
-#         password = form.password.data
-#
-#         if len(first_name) == 0\
-#                 or len(last_name) == 0\
-#                 or len(email) == 0\
-#                 or len(password) < 4:
-#             error = "Please supply an email and password"
-#         else:
-#             staff = Staff(first_name=first_name,
-#                           last_name=last_name,
-#                           email=email,
-#                           password=password)
-#             db.session.add(staff)
-#             db.session.commit()
-#             return 'Thank you'
-#     return render_template('register_staff.html', title='Register New Staff', message= error, form=form)
-
-# @app.route('/register_staff', methods=['GET', 'POST'])
-# def register_staff():
-#     error = ""
-#     form = StaffForm()
-#
-#     if request.method == 'POST':
-#         first_name = form.first_name.data
-#         last_name = form.last_name.data
-#         email = form.email.data
-#         password = form.password.data
-#
-#         if len(first_name) == 0\
-#                 or len(last_name) == 0\
-#                 or len(email) == 0\
-#                 or len(password) < 4:
-#             error = "Please supply an email and password"
-#         else:
-#             staff = Staff(first_name=first_name,
-#                           last_name=last_name,
-#                           email=email,
-#                           password=password)
-#             db.session.add(staff)
-#             db.session.commit()
-#             return 'Thank you'
-#     return render_template('register_staff.html', title='Register New Staff', message= error, form=form)
-
 
 # ACCESSING A LIST OF CURRENT STAFF
 
@@ -484,14 +251,6 @@ def show_staff():
     #     print(customer)
     return render_template('staff_list.html', staff=staff, message=error)
 
-# @app.route('/staff_list', methods=['GET'])
-# def show_staff():
-#     error = ""
-#     staff = Staff.query.all()
-#     if len(staff) == 0:
-#         error = "There are no people to display"
-#         print(staff)
-#     return render_template('staff_list.html', staff=staff, message=error)
 
 # DELETE STAFF ACCOUNTS - currently provides error message 'method not allowed'
 # @app.route('/staff/<int:staff_id>', methods=['DELETE'])
@@ -508,7 +267,6 @@ def show_staff():
 
 # REGISTERING A NEW PLANT:
 
-# not yet complete, needs rest of the fields filling in
 
 @app.route('/plant_form', methods=['GET', 'POST'])
 def plant_form():
@@ -668,3 +426,113 @@ def delete_session():
 
     flash(f' You have logged out!', 'success')
     return render_template('home.html', title='Home', form=form, message=error,)
+
+
+# Victoria's code
+# def register_basic_form():
+#     error = ""
+#     form = BasicForm()
+#
+#     if request.method == 'POST':
+#         first_name = form.first_name.data
+#         last_name = form.last_name.data
+#
+#         if len(first_name) == 0 or len(last_name) == 0:
+#             error = "Please supply both first and last name"
+#         else:
+#             person = Person(first_name=first_name, last_name=last_name)
+#             db.session.add(person)
+#             db.session.commit()
+#             return 'Thank you!'
+#     return render_template('home.html', form=form, message=error, title='home')
+
+# @app.route('/cars', methods=['GET'])
+# def show_cars():
+#     error = ""
+#     cars = Car.query.all()
+#     if len(cars) == 0:
+#         error = "There are no cars to display"
+#         print(cars)
+#     return render_template('cars.html', cars=cars, message=error, title="Car")
+
+# @app.route('/people', methods=['GET'])
+# def show_people():
+#     error = ""
+#     people = Person.query.all()
+#     if len(people) == 0:
+#         error = "There are no people to display"
+#         print(people)
+#     return render_template('people.html', people=people, message=error)
+#
+#
+# @app.route('/people/<int:person_id>', methods=['GET'])
+# def show_person(person_id):
+#     error = ""
+#     # use filter_by for any column
+#     # person = Person.query.filter_by(id=person_id).first()
+#     #  use get for the PK
+#     person = Person.query.get(person_id)
+#
+#     # simpsons = Person.query.filter_by(last_name="simpson").all()
+#
+#     # to sort
+#     # simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name).all()
+#     # descending sort
+#     # simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name.desc()).all()
+#     # limit to top 2 simpsons
+#     simpsons = Person.query.filter_by(last_name="simpson").order_by(Person.first_name).limit(2).all()
+#     if not person:
+#         error = "There is no person with ID: " + str(person_id)
+#         print(person)
+#     return render_template('person.html', person=person, message=error, title="Person", family=simpsons)
+#
+#
+# @app.route('/people/<int:person_id>', methods=['PUT'])
+# def update_person(person_id):
+#     error = ""
+#     person = Person.query.get(person_id)
+#     person.last_name = "Flanders"
+#     db.session.commit()
+#     if not person:
+#         error = "There is no person with ID: " + str(person_id)
+#         print(person)
+#     return render_template('person.html', person=person, message=error, title="Person", family=[])
+#
+#
+# @app.route('/people/<int:person_id>/<string:new_last_name>', methods=['PUT'])
+# def update_person_with_name(person_id, new_last_name):
+#     error = ""
+#     person = Person.query.get(person_id)
+#     person.last_name = new_last_name
+#     db.session.commit()
+#     if not person:
+#         error = "There is no person with ID: " + str(person_id)
+#         print(person)
+#     return render_template('person.html', person=person, message=error, title="Updated Person", family=[])
+#
+#
+# @app.route('/people/<int:person_id>', methods=['DELETE'])
+# def delete_person(person_id):
+#     error = ""
+#     person = Person.query.get(person_id)
+#     db.session.delete(person)
+#     db.session.commit()
+#     people = Person.query.all()
+#     if not person:
+#         error = "There is no person with ID: " + str(person_id)
+#         # print(person)
+#     return render_template('people.html', people=people, message=error, title="People")
+#
+#
+# @app.route('/personandcars/<int:person_id>', methods=['GET'])
+# def people_and_cars(person_id):
+#     error = ""
+#     person = Person.query.get(person_id)
+#     # cars= person.cars
+#     if not person:
+#         error = "There is no person with ID: " + str(person_id)
+#         print(person)
+#         # print(person_and_carinfo)
+#     return render_template('person_and_cars.html', person=person, message=error, title="Person and Car Info")
+#
+#     # return render_template('home.html', form=form, message=error)
